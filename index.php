077031be27f6c749e72cf32482e96aff139841b9
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-06 11:03:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <p></p>
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="img/me/shutterstock_1776352715.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
+            </div>
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <img src="img/me/1_96.jpg" class="img-fluid" width="100%" height="100%"/>
+                        </div>
+                        <div class="row">
+                            <img src="img/me/images.jpg" class="img-fluid" width="100%" height="100%"/>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="cat1.jpg" class="img-fluid rounded-circle" style="height: 100%;width: 100%;"/>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="img/me/shutterstock_407493787-e1482490925134.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="img/me/shutterstock_610935047.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
