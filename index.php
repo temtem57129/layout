c905ddfe36ff9df1bac28257f71d72201229499c
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-06 11:16:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,28 +12,28 @@
         <p></p>
         <div class="row">
             <div class="col-sm-6">
-                <img src="img/me/shutterstock_1776352715.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
+                <img src="www/img/me/shutterstock_1776352715.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
             </div>
             <div class="col-sm-6">
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="row">
-                            <img src="img/me/1_96.jpg" class="img-fluid" width="100%" height="100%"/>
+                            <img src="www/img/me/1_96.jpg" class="img-fluid" width="100%" height="100%"/>
                         </div>
                         <div class="row">
-                            <img src="img/me/images.jpg" class="img-fluid" width="100%" height="100%"/>
+                            <img src="www/img/me/images.jpg" class="img-fluid" width="100%" height="100%"/>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <img src="cat1.jpg" class="img-fluid rounded-circle" style="height: 100%;width: 100%;"/>
+                        <img src="www/img/me/cat1.jpg" class="img-fluid rounded-circle" style="height: 100%;width: 100%;"/>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
-                        <img src="img/me/shutterstock_407493787-e1482490925134.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
+                        <img src="www/img/me/shutterstock_407493787-e1482490925134.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
                     </div>
                     <div class="col-md-6">
-                        <img src="img/me/shutterstock_610935047.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
+                        <img src="www/img/me/shutterstock_610935047.jpg" class="img-fluid" style="height: 100%;width: 100%;"/>
                     </div>
                 </div>
             </div>
